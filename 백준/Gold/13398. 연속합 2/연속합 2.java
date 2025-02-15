7d5c1d61958a7f019c555c97ceecd5367cfb2862
Sunishae <130846629+sunishae@users.noreply.github.com>
2025-02-15 23:08:42 +0900

    [Gold V] Title: 연속합 2, Time: 292 ms, Memory: 29064 KB -BaekjoonHub

---
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        long[][] dp = new long[n][2];
+        long max;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0][0] = dp[0][1] = max = arr[0];
+
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0] + arr[i], arr[i]);
+            dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + arr[i]);
+            max = Math.max(max, Math.max(dp[i][0], dp[i][1]));
+        }
+
+        bw.write(max +  "\n");
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
